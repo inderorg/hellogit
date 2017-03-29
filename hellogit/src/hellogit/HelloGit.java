2a1c22a1c95c08525212fed1ad1d8c6622356770
inder.patliya <inder.patliya@247-inc.com>
2017-03-29 20:09:21 +0530

    first commit to local

---
+++ hellogit/src/hellogit/HelloGit.java
@@ -0,0 +1,14 @@
+package hellogit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		extracted();
+
+	}
+
+	private static void extracted() {
+		System.out.println(" Hello git ");
+	}
+
+}
